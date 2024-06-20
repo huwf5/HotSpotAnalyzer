ce1032a9c671c5a5d95f39aded9f0d978cbda741
brandonlv <2659827073@qq.com>
2024-06-20 10:27:44 +0800

    feat: web

--- web/src/layouts/components/Footer/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="footer flx-center">
-    <a href="https://sse.sysu.edu.cn/" target="_blank"> 2024 © HotSpot-Analyzer By SSE. </a>
-  </div>
-</template>
-
-<style scoped lang="scss">
-@import "./index.scss";
-</style>
